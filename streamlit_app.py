d3c2c4c9678e65cfb3c997290056e5bfdf2f7e5e
Sandy Zhu <113383101+sandyzjt@users.noreply.github.com>
2022-09-15 16:18:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,8 @@ streamlit.text(my_data_row)
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_rows)
+
+
+streamlit.write('Thanks for adding', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
